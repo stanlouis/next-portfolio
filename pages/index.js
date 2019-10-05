826501002394201b53ead5d2087a9990beccc017
Stanley Louis <stanlouis@msn.com>
2019-10-05 06:51:57 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel='icon' href='/static/favicon.ico' importance='low' />
+    </Head>
+    <div>
+        <h1>Home Page</h1>
+    </div>
+
+
+  </div>
+)
+
+export default Home
